f0a2fdd1c4ca5df93b0d0fdcfbe5c57208aa10ef
Scott <scott@sconklin.com>
2020-05-23 11:48:45 -0500

    removed hardcoded passwords using env variables

--- build/mssql/entrypoint.sh
+++ build/mssql/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # Run Microsoft SQl Server and initialization script (at the same time)
 echo "running db intialization...." 
-echo $1
-./run-initialization.sh $1 & /opt/mssql/bin/sqlservr 
+./run-initialization.sh & /opt/mssql/bin/sqlservr 
